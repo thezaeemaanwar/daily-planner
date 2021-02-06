91ce4b39f50874a85b21f1154e1b4874ddb51208
Zaeema Anwar <thezaeemaanwar@gmail.com>
2021-02-06 16:52:03 +0500

    👌IMPROVE

--- backend/db.js
+++ backend/db.js
@@ -4,29 +4,32 @@ let db;
 const DBStart = async () => {
   console.log("DB server connecting...");
   const client = await MongoClient.connect(
-    "mongodb://quizdom:quizdom-mongodb@cluster0-shard-00-00.lecax.mongodb.net:27017,cluster0-shard-00-01.lecax.mongodb.net:27017,cluster0-shard-00-02.lecax.mongodb.net:27017/quizdom-project?ssl=true&replicaSet=atlas-hmlbn7-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "mongodb+srv://nodejs-mongodb:nodejs-mongodb@cluster0.ulgkf.mongodb.net/usersDB?retryWrites=true&w=majority",
+
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   );
   console.log("DB Connected Successfully.");
-  db = client.db("quizdom-project");
+  db = client.db("daily-planner-app");
 };
 DBStart();
 
 const withDB = async (operations, res) => {
   try {
     await operations(db);
-    client.close();
+    // client.close();
   } catch (error) {
-    res.status(500).json({ message: "Error Connecting to db ", error });
+    // res.status(500).json({ message: "Error Connecting to db ", error });
+    console.log("error in with db : ", error);
   }
 };
 
 const createUser = async (uid, res) => {
   await withDB(async (db) => {
     const user = await db.collection("users").findOne({ uid: uid });
+    console.log(user);
     if (!user) {
       const result = await db.collection("users").insertOne({ uid });
       console.log("created user");
